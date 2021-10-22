523f544085fcadbfdd7f9a8c0a86b4f99e39f53e
Rafael Barros <rafaelbarros.df@gmail.com>
2021-10-22 00:54:05 -0300

    :tada:  iniciando o projeto

---
+++ src/test/java/com/apirest/webflux/WebfluxApplicationTests.java
@@ -0,0 +1,14 @@
+package com.apirest.webflux;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
