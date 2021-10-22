9ac10f82d839f11d9a509acca4d7992a58b6be85
Rafael Barros <rafaelbarros.df@gmail.com>
2021-10-22 01:56:14 -0300

    :construction: criando um serviço reativo funcional

--- src/main/java/com/apirest/webflux/controller/PlaylistController.java
+++ src/main/java/com/apirest/webflux/controller/PlaylistController.java
@@ -8,11 +8,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-@RestController
+// @RestController
 @RequiredArgsConstructor
 @RequestMapping("playlists")
 public class PlaylistController {
---
+++ src/main/java/com/apirest/webflux/handler/PlaylistHandler.java
@@ -0,0 +1,44 @@
+package com.apirest.webflux.handler;
+
+
+import com.apirest.webflux.document.Playlist;
+import com.apirest.webflux.service.PlaylistService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.web.reactive.function.BodyInserters.fromPublisher;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+
+@Component
+@RequiredArgsConstructor
+public class PlaylistHandler {
+
+    private final PlaylistService service;
+
+    public Mono<ServerResponse> findAll(final ServerRequest request) {
+        return ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(service.findAll(), Playlist.class);
+
+    }
+
+    public Mono<ServerResponse> findById(final ServerRequest request) {
+        final String id = request.pathVariable("id");
+        return ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(service.findById(id), Playlist.class);
+
+    }
+
+    public Mono<ServerResponse> save(final ServerRequest request) {
+        final Mono<Playlist> playlist = request.bodyToMono(Playlist.class);
+        return ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(fromPublisher(playlist.flatMap(service::save), Playlist.class));
+    }
+}
